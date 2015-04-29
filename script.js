00129d3325d6624fd7122330f858d14e94292d84
Sara OC <saramoo@hotmail.com>
2015-04-29 20:31:34 +0100

    use focus to return cursor to text box

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     $('#add-to-list').on('click', function addToList() {
         var item = $("#item").val();
         $("ol#items").append("<li>" + item + "</li>");
+        $("#item").val("");
+        $("#item").focus();
     });
 
 });
