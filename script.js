2f4cd63ffc595d6ad6e1ed2a523675f2d7e52066
Sara OC <saramoo@hotmail.com>
2015-04-29 20:55:52 +0100

    change item from pending to done

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ $(document).ready(function () {
 
     $('#add-to-list').on('click', function addToList() {
         var item = $("#item").val();
-        $("ol#items").append("<li>" + item + "</li>");
+        $("ol#items").append("<li>" + item + "<span class='label pending'>Pending</span></li>");
         $("#item").val("");
         $("#item").focus();
+    });
+
+    $(document).on('click', '.pending', function () {
+        var parentNode = $(this).parent();
+        $(this).addClass('completed');
+        $(this).removeClass('pending');
+        parentNode.append("<span class='label success'>Done!</span>");
+        console.log(this);
     });
 
 });
